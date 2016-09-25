287c949d927585de1b279596c24bff122064d5fe
Carsten Sandtner <mail@casa-rock.de>
2016-09-25 11:34:28 +0200

    update cache constantly

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME_STATIC = 'code.talks-v1';
+var CACHE_NAME_STATIC = 'code.talker-v1';
 
 self.addEventListener('install', function(event) {
     event.waitUntil(
@@ -39,41 +39,28 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('fetch', function(event) {
     var requestURL = new URL(event.request.url);
 
-    event.respondWith(
-        caches.match(event.request)
-        .then(function(response) {
-
-            // we have a copy of the response in our cache, so return it
-            if (response) {
-                console.log("Response from cache");
-                return response; //no network request necessary
-            }
-
-            var fetchRequest = event.request.clone();
-
-            return fetch(fetchRequest).then(
-                function(response) {
-                    // Chache what ever we need.
-                    if (response.type === "basic" && response.status === 200) {
-                        console.log("New valid request. New cache: " + CACHE_NAME_STATIC);
-
-                        var responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME_STATIC)
-                            .then(function(cache) {
-                                var cacheRequest = event.request.clone();
-                                cache.put(cacheRequest, responseToCache);
-                            });
-                    }
-                    // response...
-                    return response;
-                }
-            ).catch(function(error) {
-                console.log("error");
-                return caches.open(CACHE_NAME_STATIC).then(function(cache) {
-                    return cache.match('offline.html');
-                });
-            });
-        })
-    );
+    event.respondWith(returnFromCacheOrFetch(event.request));
 });
+
+function returnFromCacheOrFetch(request) {
+    const cachePromise = caches.open(CACHE_NAME_STATIC);
+    const matchPromise = cachePromise.then(function(cache) {
+        return cache.match(request);
+    });
+
+    return Promise.all([cachePromise, matchPromise]).then(function([cache, cacheResponse]) {
+        // Kick off the update request
+        const fetchPromise = fetch(request).then(function(fetchResponse) {
+            // Cache the updated file and then return the response
+            cache.put(request, fetchResponse.clone());
+            return fetchResponse;
+        });
+            // return the cached response if we have it, otherwise the result of the fetch.
+        return cacheResponse || fetchPromise;
+    }).catch(function(error) {
+        console.log("error");
+        return caches.open(CACHE_NAME_STATIC).then(function(cache) {
+            return cache.match('offline.html');
+        });
+    });
+}
