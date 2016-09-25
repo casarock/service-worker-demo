47ad94a9882240a445cf0dfb81e0ef81b9b198f2
Carsten Sandtner <mail@casa-rock.de>
2016-09-25 09:21:54 +0200

    offline Fallback

--- sw.js
+++ sw.js
@@ -1,1 +1,31 @@
-console.log("registered");
+self.addEventListener('install', function(event) {
+    // Put 'offline.html' page into cache
+    var offline = new Request('offline.html');
+    event.waitUntil(
+        fetch(offline).then(function(response) {
+            return caches.open('offline').then(function(cache) {
+                return cache.put(offline, response);
+            });
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    var request = event.request;
+
+    if (request.method === 'GET') {
+        event.respondWith(
+
+            // `fetch()` throws exception when unreachable
+            // not for valid requests.
+            fetch(request).catch(function(error) {
+                console.log('Fetch returned: ' + error);
+
+                // Error! We need to serve offline.html (when already cached)!
+                return caches.open('offline').then(function(cache) {
+                    return cache.match('offline.html');
+                });
+            })
+        );
+    }
+});
