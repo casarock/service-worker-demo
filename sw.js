7fe16b5ff95dbf333084893fae9b18b38d6d8710
Carsten Sandtner <mail@casa-rock.de>
2016-09-25 10:52:50 +0200

    cache pages

--- sw.js
+++ sw.js
@@ -1,31 +1,79 @@
+var CACHE_NAME_STATIC = 'code.talks-v1';
+
 self.addEventListener('install', function(event) {
-    // Put 'offline.html' page into cache
-    var offline = new Request('offline.html');
     event.waitUntil(
-        fetch(offline).then(function(response) {
-            return caches.open('offline').then(function(cache) {
-                return cache.put(offline, response);
-            });
+        // Cache static
+        caches.open(CACHE_NAME_STATIC).then(function(cache) {
+
+            return cache.addAll([
+                '/webpage.html',
+                '/css/',
+                '/css/layouts/',
+                '/img/',
+                '/js/app.js',
+                'offline.html'
+            ]);
+        })
+    )
+});
+
+
+
+self.addEventListener('activate', function(event) {
+    var cacheWhitelist = [CACHE_NAME_STATIC];
+
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    console.log("Found: ", cacheName);
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
 
 self.addEventListener('fetch', function(event) {
-    var request = event.request;
+    var requestURL = new URL(event.request.url);
 
-    if (request.method === 'GET') {
-        event.respondWith(
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
 
-            // `fetch()` throws exception when unreachable
-            // not for valid requests.
-            fetch(request).catch(function(error) {
-                console.log('Fetch returned: ' + error);
+            // we have a copy of the response in our cache, so return it
+            if (response) {
+                console.log("Response from cache");
+                return response; //no network request necessary
+            }
 
-                // Error! We need to serve offline.html (when already cached)!
-                return caches.open('offline').then(function(cache) {
+            var fetchRequest = event.request.clone();
+
+            return fetch(fetchRequest).then(
+                function(response) {
+                    // Chache what ever we need.
+                    if (response.type === "basic" && response.status === 200) {
+                        console.log("New valid request. New cache: " + CACHE_NAME_STATIC);
+
+                        var responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME_STATIC)
+                            .then(function(cache) {
+                                var cacheRequest = event.request.clone();
+                                cache.put(cacheRequest, responseToCache);
+                            });
+                    }
+                    // response...
+                    return response;
+                }
+            ).catch(function(error) {
+                console.log("error");
+                return caches.open(CACHE_NAME_STATIC).then(function(cache) {
                     return cache.match('offline.html');
                 });
-            })
-        );
-    }
+            });
+        })
+    );
 });
